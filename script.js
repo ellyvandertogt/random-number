440ca232882ec8ecf74a4d01e7efac5065f1b8d6
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2022-12-16 21:37:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,18 @@
 var randNum;
 //randNum = Math.floor(Math.random() * 6);//random 0 to 6
 //randNum = Math.floor(Math.random() * 6) + 1;//random 1 to 6
-randNum = Math.floor(Math.random() * 100) + 1;//random 1 to 6
+//randNum = Math.floor(Math.random() * 100) + 1;//random 1 to 6
 
+//Create a random number with function
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+randNum = getRandomNum(1 , 100);
+console.log(randNum);
+randNum = getRandomNum(1 , 100);
+console.log(randNum);
+randNum = getRandomNum(1 , 100);
 console.log(randNum);
 
 
