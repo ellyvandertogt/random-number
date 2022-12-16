722cb26dbaf010fb1ee69999e23e20c124dd2337
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2022-12-16 21:05:18 +0100

    first submit

---
+++ script.js
@@ -0,0 +1,12 @@
+//Math.random() = Generates a seudo-random between 0 and 1.
+
+var randNum;
+//randNum = Math.floor(Math.random() * 6);//random 0 to 6
+//randNum = Math.floor(Math.random() * 6) + 1;//random 1 to 6
+randNum = Math.floor(Math.random() * 100) + 1;//random 1 to 6
+
+console.log(randNum);
+
+
+
+
